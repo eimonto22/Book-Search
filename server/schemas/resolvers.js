7de3e8328552388db14d34ac10da985dec2e8d81
eddieim <eisaiahmo22@gmail.com>
2024-06-24 20:07:32 -0600

    added coded for resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,39 +1,87 @@
-const typeDefs = `
-type User {
-    _id: ID!
-    username: String!
-    email: String!
-	bookCount: Int
-    savedBooks: [Book]
-}
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
-# Set up an Auth type to handle returning data from a profile creating or user login
-type Auth {
-    token: ID!
-    user: User
-}
+const resolvers = {
+	Query: {
+		me: async (parent, { userId }) => {
+			return User.findOne({ _id: userId });
+		},
+		users: async () => {
+			return User.find();
+		},
+	},
 
-type Book {
-	bookId: ID!
-	authors: [String]
-	description: String
-	title: String
-	image: String
-	link: String
-}
+	Mutation: {
+		addUser: async (parent, { username, email, password }) => {
+			console.log(`username: ${username}, email: ${email}, password: ${password}`)
 
-type Query {
-	me(userId: String!): User
-	users: [User]
-}
+			try {
+				const user = await User.create({ username, email, password });
+				console.log('user created: ', user)
+				const token = signToken(user);
+				console.log('token created: ', token);
 
-type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+				return { token, user };
+			} catch (err) {
+				console.log(err.message);
+				throw err
+			}
+		},
 
-	saveBook(userId: ID!, bookId: ID!, authors: [String], description: String, title: String, image: String, link: String): User
-	removeBook(userId: ID!, bookId: ID!): User
-}
-`;
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
 
-module.exports = typeDefs;
+			if (!user) {
+				throw AuthenticationError;
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw AuthenticationError;
+			}
+
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		saveBook: async (parent, { userId, bookId, authors, description, title, image, link }) => {
+			try {
+			const updatedUser = await User.findOneAndUpdate(
+				{ _id: userId },
+				{ 
+				$addToSet: { 
+					savedBooks: {
+					bookId,
+					authors,
+					description,
+					title,
+					image,
+					link
+					} 
+				} 
+				},
+				{ new: true, runValidators: true }
+			);
+			return updatedUser;
+			} catch (err) {
+			console.log(err);
+			throw new Error('Could not save book');
+			}
+		},
+
+		removeBook: async (parent, { userId, bookId }) => {
+			const updatedUser = await User.findOneAndUpdate(
+				{ _id: userId },
+				{ $pull: { savedBooks: { bookId: bookId } } },
+				{ new: true }
+			);
+			if (!updatedUser) {
+				throw new Error('Could not find user with this id!');
+			}
+			return updatedUser;
+		},
+	}
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+	bookCount: Int
+    savedBooks: [Book]
+}
+
+# Set up an Auth type to handle returning data from a profile creating or user login
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Book {
+	bookId: ID!
+	authors: [String]
+	description: String
+	title: String
+	image: String
+	link: String
+}
+
+type Query {
+	me(userId: String!): User
+	users: [User]
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+	saveBook(userId: ID!, bookId: ID!, authors: [String], description: String, title: String, image: String, link: String): User
+	removeBook(userId: ID!, bookId: ID!): User
+}
+`;
+
+module.exports = typeDefs;
